cb55761d3d0c59d84f2f932adcf64d6604fa9fec
Freddy Hernandez <freddy.hernandez@fandm.edu>
2019-05-27 14:25:30 -0400

    stored questions + created a questions component

---
+++ src/app/questions.ts
@@ -0,0 +1,42 @@
+export var gotQuestions: any[] = [
+  {
+    question: "What is Jon Snow's real name?",
+    answers: {
+      a: "Aemon Targaryen",
+      b: "Aerys Targaryen",
+      c: "Aegon Targaryen",
+      d: "Viserys Targaryen"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who wins the iron throne?",
+    answers: {
+      a: "Brandon Stark",
+      b: "Jon Snow",
+      c: "Tyrion Lannister",
+      d: "Daenerys Targaryen"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Who originally owned the dagger that killed the Knight King?",
+    answers: {
+      a: "Varys",
+      b: "Petyr Baelish",
+      c: "Brandon Stark",
+      d: "Cersei Lannister"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "What weapon did Tyrion use to kill Tywin Lannister?",
+    answers: {
+      a: "Spear",
+      b: "Sword",
+      c: "Dagger",
+      d: "Crossbow"
+    },
+    correctAnswer: "d"
+  }
+]
