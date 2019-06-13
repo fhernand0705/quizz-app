c75a4f27b81181bb64ab5492c63a5c9e9dec246d
Freddy Hernandez <freddy.hernandez@fandm.edu>
2019-06-13 13:26:29 -0400

    retrying

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ const path = require('path');
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist/quizapp'));
+//app.use(express.static(__dirname + '/dist/quizapp'));
+app.use(express.static(`${__dirname}/front-end/dist/`));
 
 // ...
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/quizapp/index.html'));
+  res.sendFile(`./front-end/dist/quizapp/index.html`);
 });
+
+//res.sendFile(path.join(`./front-end/dist/quizapp/index.html`));
 
 // Start the app by listening on the default
 // Heroku port
